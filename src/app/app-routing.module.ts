d1145c49a9028697da40f8856baf4aad4ea17f4a
ElLuchoMan <luchoman96@gmail.com>
2021-01-11 16:56:46 -0500

    Enrutamiento hecho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
+import { CreateEmpleadosComponent } from './components/create-empleados/create-empleados.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'listarEmpleados',pathMatch:'full'},
+  {path: 'listarEmpleados', component: ListEmpleadosComponent},
+  {path: 'crearEmpleados', component: CreateEmpleadosComponent},
+  {path:'**',redirectTo:'listarEmpleados',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
