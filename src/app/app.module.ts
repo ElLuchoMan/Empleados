89cf20d716fcec230c2607180c952ae68411d04c
ElLuchoMan <luchoman96@gmail.com>
2021-01-11 15:55:16 -0500

    navbar creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
+import { CreateEmpleadosComponent } from './components/create-empleados/create-empleados.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmpleadosComponent,
+    CreateEmpleadosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/create-empleados/create-empleados.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-empleados',
+  templateUrl: './create-empleados.component.html',
+  styleUrls: ['./create-empleados.component.css']
+})
+export class CreateEmpleadosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
